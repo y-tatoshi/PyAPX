6e4724886abf9c2624b6b454b27e5edbc10cc452
a-ksb <209758726+a-ksb@users.noreply.github.com>
2026-04-14 15:16:09 +0900

    v1.0.1

--- pyapx/__init__.py
+++ pyapx/__init__.py
@@ -4,4 +4,4 @@ PyAPX - Python Toolkit for Atomic Configuration Pattern Exploration
 A toolkit that integrates DFT codes with Bayesian optimization to explore stable atomic configurations in materials.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
